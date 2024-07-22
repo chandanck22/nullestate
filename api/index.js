276bfecca7f8492b0ff1868ee57c6bdec77917cd
chandan <chandan7979ck@gmail.com>
2024-07-22 12:31:24 +0530

    save signup in DB

--- api/index.js
+++ api/index.js
@@ -1,21 +1,27 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('Database is connected');
 }).catch((err) => {
   console.log(err);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
+// Start the server
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
