61d418daa7512501697e5bda8f7a6bf9726ee3fc
chandan <chandan7979ck@gmail.com>
2024-08-04 09:33:06 +0530

    added middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,16 @@
 import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json({ message: 'User created successfully' });
+  const hashedPassword = await bcryptjs.hash(password, 12);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    // res.status(500).json({ message: 'Something went wrong' });
+    // res.status(500).json({ message: error.message });
+    next(error);  // this error coming from middleware in api/index.js
+  }
 };
--- api/index.js
+++ api/index.js
@@ -13,6 +13,17 @@ app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
+// middleware
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  });
+});
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('Database is connected');
