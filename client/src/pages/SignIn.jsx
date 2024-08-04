0e25a06893cfb685177af56776889d34bdfb0626
chandan <chandan7979ck@gmail.com>
2024-08-04 11:51:56 +0530

    added redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import {useDispatch, useSelector} from 'react-redux';
+import { signInSuccess, signInStart, signInFailure } from "../redux/user/userSlice";
 
 const SignIn = () => {
 
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error} = useSelector(state => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -19,7 +20,7 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       // added proxy "localhost:3000" inside vite.config.js
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
@@ -31,16 +32,13 @@ const SignIn = () => {
       const data = await res.json();
       // console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+        dispatch(signInFailure(error.message));
     }
   };
 
